bc8c8ff611d240cfda4ae227ef5040116edbc055
fxOne <fxOne@users.noreply.github.com>
2022-01-24 20:08:40 +0100

    add matches

--- data/lichessStreamerChampionship/Brackets.ts
+++ data/lichessStreamerChampionship/Brackets.ts
@@ -3,11 +3,27 @@ import { Bracket } from '../Interfaces';
 export const brackets: Bracket[][] = [
   [
     {
+      matchId: 3,
+      predecessors: null,
+    },
+    {
+      matchId: 7,
+      predecessors: null,
+    },
+    {
+      matchId: 5,
+      predecessors: null,
+    },
+    {
       matchId: 1,
       predecessors: null,
     },
     {
-      matchId: 3,
+      matchId: 9,
+      predecessors: null,
+    },
+    {
+      matchId: 10,
       predecessors: null,
     },
     {
@@ -19,7 +35,7 @@ export const brackets: Bracket[][] = [
       predecessors: null,
     },
     {
-      matchId: 5,
+      matchId: 8,
       predecessors: null,
     },
     {
@@ -29,36 +45,70 @@ export const brackets: Bracket[][] = [
   ],
   [
     {
-      matchId: 7,
-      predecessors: [null, 1],
+      matchId: 11,
+      predecessors: [null, 3],
     },
     {
-      matchId: 8,
-      predecessors: [3, 2],
+      matchId: 15,
+      predecessors: [null, 7],
     },
     {
-      matchId: 10,
-      predecessors: [5, 6],
+      matchId: 13,
+      predecessors: [null, 5],
     },
     {
-      matchId: 9,
+      matchId: 17,
+      predecessors: [1, 9],
+    },
+    {
+      matchId: 18,
+      predecessors: [2, 10],
+    },
+    {
+      matchId: 14,
+      predecessors: [6, null],
+    },
+    {
+      matchId: 16,
+      predecessors: [8, null],
+    },
+    {
+      matchId: 12,
       predecessors: [4, null],
     },
   ],
   [
     {
-      matchId: 11,
-      predecessors: [7, 8],
+      matchId: 19,
+      predecessors: [11, 15],
     },
     {
-      matchId: 12,
-      predecessors: [9, 10],
+      matchId: 21,
+      predecessors: [13, 17],
+    },
+    {
+      matchId: 20,
+      predecessors: [12, 16],
+    },
+    {
+      matchId: 22,
+      predecessors: [14, 18],
     },
   ],
   [
     {
-      matchId: 13,
-      predecessors: [11, 12],
+      matchId: 23,
+      predecessors: [19, 21],
+    },
+    {
+      matchId: 24,
+      predecessors: [20, 22],
+    },
+  ],
+  [
+    {
+      matchId: 25,
+      predecessors: [23, 24],
     },
   ],
 ];
